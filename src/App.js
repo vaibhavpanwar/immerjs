6ece4f661570f0a7a6f504cad4b94ec820727d78
Vaibhav Panwar <vaibhavpanwar1402@gmail.com>
2021-08-07 01:20:28 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import produce from "immer";
 
 function App() {
+  const [todos, setTodos] = useState(["shopping", "workout", "study"]);
+  const [todo, setTodo] = useState("");
+  const [editTodo, setEditTodo] = useState(null);
+
+  const clearForm = () => {
+    setTodo("");
+    setEditTodo(null);
+  };
+
+  //WITHOUT IMMER
+
+  // const addTodo = () => {
+  //   setTodos((prevState) => [todo, ...prevState]);
+  //     clearForm()
+  // };
+
+  //WITH IMMER
+
+  const addTodo = () => {
+    setTodos(
+      produce((draftState) => {
+        draftState.unshift(todo);
+      })
+    );
+    clearForm();
+  };
+
+  //WITHOUT IMMER
+
+  // const deleteTodo = (i) => {
+  //   setTodos(todos?.filter((item, index) => index !== i));
+  //   clearForm();
+  // };
+
+  //WITH IMMER
+
+  const deleteTodo = (i) => {
+    setTodos(
+      produce((draftState) => {
+        draftState.splice(i, 1);
+      })
+    );
+  };
+
+  //WITHOUT IMMER
+
+  // const updateTodo = () => {
+  //   setTodos(todos?.map((item, index) => (index !== editTodo ? item : todo)));
+  //     clearForm()
+  // };
+
+  //WITH IMMER
+
+  const updateTodo = () => {
+    setTodos(
+      produce((draftState) => {
+        draftState[editTodo] = todo;
+      })
+    );
+    clearForm();
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    editTodo || editTodo === 0 ? updateTodo() : addTodo();
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <form onSubmit={submitHandler}>
+          <input
+            type="text"
+            value={todo}
+            required
+            onChange={(e) => setTodo(e.target.value)}
+            placeholder={`whats in your list today`}
+          />{" "}
+        </form>
+        {todos.map((item, index) => (
+          <p className="todo-item">
+            {item}{" "}
+            <span>
+              {" "}
+              <span
+                onClick={() => {
+                  setTodo(item);
+                  setEditTodo(index);
+                }}
+              >
+                Edit
+              </span>{" "}
+              &nbsp; <span onClick={() => deleteTodo(index)}>Delete</span>
+            </span>
+          </p>
+        ))}
+      </div>
+    </>
   );
 }
 
